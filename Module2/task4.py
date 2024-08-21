061456b04857a0b3dd0b51bdde5dc14899fe7712
Yehor <te35603560@gmail.com>
2024-08-21 10:35:53 +0300

    task4,5 added

---
+++ Module2/task4.py
@@ -0,0 +1,12 @@
+print("This is program that output sum, product and average of three numbers. You should only three INTEGER numbers. Our program will do all calculations!")
+x=int(input("Please enter first number: "))
+y=int(input("Please enter second number: "))
+z=int(input("Please enter third number: "))
+
+sumofthree=x+y+z
+product=x*y*z
+average=sumofthree/3
+
+print("sum=", sumofthree)
+print("product=", product)
+print("average=", average)
---
+++ Module2/task5.py
@@ -0,0 +1,20 @@
+
+
+print("This is a program that converts the input to full kilograms and grams and outputs the result to the user.")
+
+talents = float(input("Enter talents: \n"))
+pounds = float(input("Enter pounds: \n"))
+lots = float(input("Enter lots: \n"))
+
+pounds = pounds + talents * 20
+lots = lots + pounds * 32
+grams = lots * 13.3
+
+kilograms = int(grams // 1000)
+grams = grams % 1000
+
+
+print(f"The weight in modern units: \n{kilograms} kilograms and {grams:.2f} grams")
+
+
+
