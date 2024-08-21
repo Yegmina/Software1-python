5a545133dcdc4b87f4685d8c4687f322c452af74
Yehor <te35603560@gmail.com>
2024-08-21 10:47:46 +0300

    task6 added

---
+++ Module2/task6.py
@@ -0,0 +1,17 @@
+from random import random
+from random import randint
+
+line=''
+randomvar = int(random()*1000)
+line=line+str(randomvar)
+
+
+print(line)
+line=''
+i=0
+while i<=4:
+    randomvar = randint(0,6)
+    line=line+str(randomvar)
+    i+=1
+
+print(line)
