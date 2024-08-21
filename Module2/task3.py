2593f0e6f96e71d889e1474916a0c769b0b2af9e
Yehor <te35603560@gmail.com>
2024-08-21 09:57:50 +0300

    task1,3 added

---
+++ Module2/task3.py
@@ -0,0 +1,7 @@
+print("This is program that output perimeter and area of the rectangle. You should only provide length and width. Our program will do all calculations!")
+length=float(input("Enter the length of the rectangle: "))
+width=float(input("Enter the width of the rectangle: "))
+perimeter=2*(length+width)
+area=length*width
+print("Perimeter=",perimeter)
+print("Area=",area)
