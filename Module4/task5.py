6bccae008510d310fe17b0c893454993abad9da5
Yehor <te35603560@gmail.com>
2024-08-23 14:07:21 +0300

    space deleted

--- Module4/task5.py
+++ Module4/task5.py
@@ -1,15 +1,10 @@
 import hashlib
-
 correct_username="python"
-
 password_hash="6c621d1a05138a7888d37d9269a9da8e2e11e4aced2f6cfd24b05ab1b9e61bb0"
 #this is sha256 hash, generated separately from this script
 
-
 def sha256(password): #function for hash calculation
     return hashlib.sha256(password.encode()).hexdigest()
-
-
 i=1
 
 while i<=5:
@@ -21,7 +16,6 @@ while i<=5:
     else:
         print("Wrong username or password, try again!")
     i=i+1
-
 print("Access denied")
 #and now a little explanation
 #why didn't I just use password==correct_password?
