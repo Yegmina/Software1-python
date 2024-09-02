e58285938f473dbf6cde752e83b3704d62ab439e
Yehor <te35603560@gmail.com>
2024-09-02 11:12:17 +0300

    module7task1 added another option of calculation (task is not clear because it does not provide input/output examples)

--- Module7/task1.py
+++ Module7/task1.py
@@ -1,25 +1,55 @@
+"""
+1
+2
+3
+
+4
+5
+6
+
+7
+8
+9
+
+10
+11
+12
+
+
 seasons=("winter", "spring", "summer", "autumn")
 
 n=int(input("Enter a month number: "))
 
 season= ((n+2)//3)-1
+
+print(seasons[season])
+"""
+
+
+"""
+12
+1
+2
+
+3
+4
+5
+
+6
+7
+8
+
+9
+10
+11
+"""
+
+seasons=("winter", "spring", "summer", "autumn")
+n=int(input("Enter a month number: "))
+season= (n//3)-4
 print(seasons[season])
 
-"""
-December - 1
-January - 2
-February - 3
 
-March - 4
-April - 5
-May - 6
 
-June - 7
-July - 8
-August - 9
 
-September - 10
-October - 11
-November - 12
-"""
 
