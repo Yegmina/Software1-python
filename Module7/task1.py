ead11c4636bf13ee703243bd5152597a3359b3c9
Yehor <te35603560@gmail.com>
2024-09-13 15:06:15 +0300

    comments place changed

--- Module7/task1.py
+++ Module7/task1.py
@@ -1,30 +1,3 @@
-"""
-1
-2
-3
-
-4
-5
-6
-
-7
-8
-9
-
-10
-11
-12
-
-
-seasons=("winter", "spring", "summer", "autumn")
-
-n=int(input("Enter a month number: "))
-
-season= ((n+2)//3)-1
-
-print(seasons[season])
-"""
-
 
 """
 12
@@ -51,5 +24,34 @@ print(seasons[season])
 
 
 
+
+
+
+"""
+1
+2
+3
+
+4
+5
+6
+
+7
+8
+9
+
+10
+11
+12
+
+
+seasons=("winter", "spring", "summer", "autumn")
+
+n=int(input("Enter a month number: "))
+
+season= ((n+2)//3)-1
+
+print(seasons[season])
+"""
 
 
